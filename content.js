9d5f35d9d5ca1570adb66def174145d761fc25dd
Kirsten Lund <kirsten.d.lund@gmail.com>
2021-03-19 22:26:44 -0400

    first commit

---
+++ content.js
@@ -0,0 +1,13 @@
+console.log("chrome ext GO");
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+  console.log(message.txt);
+  if (message.txt === "hello") {
+    let paragraphs = document.getElementsByTagName('p');
+    for (el of paragraphs) {
+    el.style['background-color'] = '#EEB4FF';
+    }
+  }
+}
