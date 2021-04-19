895068a0736014d3887aaebe3520c84769f58c7e
Kirsten Lund <kirsten.d.lund@gmail.com>
2021-04-18 22:33:29 -0400

    MOAR MULCH

--- content.js
+++ content.js
@@ -1,13 +1,36 @@
 console.log("chrome ext GO");
 
-chrome.runtime.onMessage.addListener(gotMessage);
+let filenames = [
+  "mulch1.png",
+  "mulch2.png",
+  "mulch3.png",
+  "mulch4.png",
+  "mulch5.png",
+  "mulch6.png",
+  "mulch7.png",
+  "mulch8.png",
+  "mulch9.png",
+  "mulch10.png",
+  "mulch11.png",
+  "mulch12.png",
+  "mulch13.png",
+  "mulch14.png",
+  "mulch15.png",
+  "mulch16.png",
+  "mulch17.png",
+  "mulch18.png",
+  "mulch19.png",
+  "mulch20.png",
+  "mulch21.png",
+  "mulch22.png",
+  "mulch23.png"
+];
 
-function gotMessage(message, sender, sendResponse) {
-  console.log(message.txt);
-  if (message.txt === "hello") {
-    let paragraphs = document.getElementsByTagName('p');
-    for (el of paragraphs) {
-    el.style['background-color'] = '#EEB4FF';
-    }
-  }
+let images = document.getElementsByTagName('img');
+for (imgElt of images) {
+  let r = Math.floor(Math.random() * filenames.length); 
+  let file = `img/${filenames[r]}`;
+  let url = chrome.extension.getURL(file);
+  imgElt.src = url;
 }
+
